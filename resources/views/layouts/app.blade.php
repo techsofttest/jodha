9930c535444ba63c6d33e2058cd31d1e649219c1
Akhil-Techsoft <techsofttest@gmail.com>
2026-06-03 09:54:18 +0530

    Favicon, Modal update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{asset('images/logo/favicon.png')}}" type="image/x-icon">
+    <link rel="icon" href="{{asset('images/logo/favicon.png')}}?v1" type="image/x-icon">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
