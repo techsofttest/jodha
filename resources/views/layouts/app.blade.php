1c2953f6a3fe2c4b2e6f5c0069cb1eef11ae3d65
Akhil-Techsoft <techsofttest@gmail.com>
2026-06-03 16:19:53 +0530

    Favicon + meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{asset('images/logo/favicon.png')}}?v1" type="image/x-icon">
+    <link rel="icon" href="{{asset('images/logo/favicon.png')}}?v1.1" type="image/x-icon">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -24,6 +24,27 @@
 <meta name="twitter:title" content="Shop Premium Furniture and Home Decor Online in India - Jodha Furniture">
 <meta name="twitter:description" content="Buy luxury furniture and home decor items online at best prices in India at Ashborn Furniture. Explore elegant designs with EMI and COD options available. Shop Now!">
     
+
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1591339188632110');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1591339188632110&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
+
 
     @yield('head_metas')
 
