32853c75f456c608b5c66d7cc6fc14b0b15c0d8c
Akhil-Techsoft <techsofttest@gmail.com>
2026-06-26 12:40:12 +0530

    Remove icon product detail

--- resources/views/pages/product-detail.blade.php
+++ resources/views/pages/product-detail.blade.php
@@ -452,15 +452,7 @@
                                     style="font-family: var(--f-body); font-size: 12px; color: var(--c-primary); font-weight: 500;">Eco-Certified
                                     Materials</span>
                             </div>
-                            <div class="d-flex align-items-center">
-                                <span class="perk-icon me-3">
-                                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-                                    </svg>
-                                </span>
-                                
-                            </div>
+                            
                         </div>
 
                     </div>
